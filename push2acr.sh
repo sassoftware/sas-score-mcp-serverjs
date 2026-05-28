ba63dfde7657735abfa4bbcb91a34b0f62df54eb
devakumaraswamy <deva.kumar@sas.com>
2026-05-28 12:46:45 -0400

    save

--- push2acr.sh
+++ push2acr.sh
@@ -2,7 +2,7 @@
 # syntax: push2acr.sh 
 # Replace viyafseditcr with your ACR name and sas-score-mcp-serverjs with your image name if different. 
 #
-docker rmi viyafseditcr.azurecr.io/sas-score-mcp-serverjs
+docker rmi viyafseditcr.azurecr.io/sas-score-mcp-serverjs-agent
 az acr login --name viyafseditcr
-docker build --no-cache --platform linux/amd64 -t  viyafseditcr.azurecr.io/sas-score-mcp-serverjs .
-docker push viyafseditcr.azurecr.io/sas-score-mcp-serverjs:latest
+docker build --no-cache --platform linux/amd64 -t  viyafseditcr.azurecr.io/sas-score-mcp-serverjs-agent .
+docker push viyafseditcr.azurecr.io/sas-score-mcp-serverjs-agent:latest
